2fe2d1b91029aad8a9f185c33f4b51a782bc22b3
IgorMontezuma20 <igorminranda17@gmail.com>
2024-01-24 21:23:43 -0300

    feat: Criando o Projects Component

---
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -0,0 +1,28 @@
+import { Component, signal } from '@angular/core';
+import { IProjects } from '../../interface/IProjects';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+public projectsArray = signal<IProjects[]>([
+  {
+    src: 'assets/img/projects/vfull.png',
+    alt: '',
+    title: "Project 1",
+    width: "100px",
+    height: "51px",
+    description: "",
+    links: [
+      {
+        name: 'Conheça o projeto',
+        href: '#',
+      }
+    ]
+  }
+])
+}
