b18ce79d1543dacb3c285d0e834cac1f0e98428c
IgorMontezuma20 <igorminranda17@gmail.com>
2024-01-26 12:28:41 -0300

    feat: Adicionando a implementação do OpenDialogComponent

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,85 +1,42 @@
-import { Component, signal } from '@angular/core';
-import { IProjects } from '../../interface/IProjects';
+import { IProjects } from './../../interface/IProjects';
+import { Component, inject, signal } from '@angular/core';
+import{ MatDialogModule } from '@angular/material/dialog';
+
+
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
-public projectsArray = signal<IProjects[]>([
-  {
-    src: 'assets/img/projects/vfull.png',
-    alt: '',
-    title: "Project 1",
-    width: "100px",
-    height: "51px",
-    description: "",
-    links: [
-      {
-        name: 'Conheça o projeto',
-        href: '#',
-      }
-    ]
-  },
-  {
-    src: 'assets/img/projects/vfull.png',
-    alt: '',
-    title: "Project 1",
-    width: "100px",
-    height: "51px",
-    description: "",
-    links: [
-      {
-        name: 'Conheça o projeto',
-        href: '#',
-      }
-    ]
-  },
-  {
-    src: 'assets/img/projects/vfull.png',
-    alt: '',
-    title: "Project 1",
-    width: "100px",
-    height: "51px",
-    description: "",
-    links: [
-      {
-        name: 'Conheça o projeto',
-        href: '#',
-      }
-    ]
-  },
-  {
-    src: 'assets/img/projects/vfull.png',
-    alt: '',
-    title: "Project 1",
-    width: "100px",
-    height: "51px",
-    description: "",
-    links: [
-      {
-        name: 'Conheça o projeto',
-        href: '#',
-      }
-    ]
-  },
-  {
-    src: 'assets/img/projects/vfull.png',
-    alt: '',
-    title: "Project 1",
-    width: "100px",
-    height: "51px",
-    description: "",
-    links: [
-      {
-        name: 'Conheça o projeto',
-        href: '#',
-      }
-    ]
-  },
+  #dialog = inject(MatDialog);
+  public projectsArray = signal<IProjects[]>([
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: '',
+      title: 'Project 1',
+      width: '100px',
+      height: '51px',
+      description: '',
+      links: [
+        {
+          name: 'Conheça o projeto',
+          href: '#',
+        },
+      ],
+    },
+  ]);
 
-])
+  public openDialog(data: IProjects){
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
 }
